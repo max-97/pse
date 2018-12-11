fa49e503311c93a18570297129156ed81490989f
Michel Bodé <michel.bode97@gmail.com>
2018-12-12 00:18:10 +0100

    Name und Beschreibung zu Adaptationsalgorithmen für gemischte Strategien hinzugefügt

--- Implementierung/src/main/java/de/swiss/model/algorithms/adaptation/MixedLinearInterpolation.java
+++ Implementierung/src/main/java/de/swiss/model/algorithms/adaptation/MixedLinearInterpolation.java
@@ -3,6 +3,9 @@ package de.swiss.model.algorithms.adaptation;
 import de.swiss.model.Simulation;
 
 public class MixedLinearInterpolation implements MixedAdaptationAlgorithm{
+
+    public static final String NAME = "";
+    public static final String DESCRIPTION = "";
     @Override
     public void adapt(Simulation sim) {
 
--- Implementierung/src/main/java/de/swiss/model/algorithms/adaptation/RandomAdaptation.java
+++ Implementierung/src/main/java/de/swiss/model/algorithms/adaptation/RandomAdaptation.java
@@ -3,6 +3,9 @@ package de.swiss.model.algorithms.adaptation;
 import de.swiss.model.Simulation;
 
 public class RandomAdaptation implements AdaptationAlgorithm, MixedAdaptationAlgorithm {
+
+    public static final String NAME = "";
+    public static final String DESCRIPTION = "";
     private final int PROBABILITY;
 
     public RandomAdaptation(int PROBABILITY) {
