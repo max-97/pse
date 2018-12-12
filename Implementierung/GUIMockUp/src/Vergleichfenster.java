52ad66afaad7b2fceacc6f8ddaafafb9bf236b08
sophie <uusqv@student.kit.edu>
2018-12-12 10:09:52 +0100

    Klassen und forms vom GUI Entwurf hinzugefügt

---
+++ Implementierung/GUIMockUp/src/Vergleichfenster.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class Vergleichfenster {
+    private JTree wählenSieEineKonfigurationTree;
+    private JRadioButton wähleFürAlleKonfigurationenRadioButton;
+    private JRadioButton zeigeDieKTeRadioButton;
+    private JFormattedTextField formattedTextField1;
+    private JTabbedPane tabbedPane1;
+}
