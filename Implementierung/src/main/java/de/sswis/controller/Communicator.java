a89fabd33b0b6fe46986b429ac6750479895b9cd
Simon Hügel <uqsbm@student.kit.edu>
2018-12-20 16:32:53 +0100

    Communicator.java angepasst

--- Implementierung/src/main/java/de/sswis/controller/Communicator.java
+++ Implementierung/src/main/java/de/sswis/controller/Communicator.java
@@ -1,10 +1,8 @@
 package de.sswis.controller;
 
 
-import de.sswis.model.Configuration;
-import de.sswis.model.Simulation;
-import de.sswis.view.model.VMConfiguration;
-import de.sswis.view.model.VMResult;
+import de.sswis.model.*;
+import de.sswis.view.model.*;
 
 /**
  * Ein Kommunikator zum Austausch von Eingaben und Ergebnissen zwischen dem Model und dem ViewModel.
@@ -13,6 +11,32 @@ import de.sswis.view.model.VMResult;
  */
 public class Communicator {
 
+    public VMCombinedStrategy parseCombinedStrategyToVMCombinedStrategy(CombinedStrategy combinedStrategy) { return null; }
+    /**
+     * Gibt zu einer Simulation die zugrundeliegende Konfiguration in einem für die Benutzeroberfläche konformen Format zurück.
+     *
+     * @param simulation die Simulation
+     * @return die der Simulation zugrundeliegende Konfiguration
+     */
+    public VMConfiguration parseSimulationToVMConfiguration(Simulation simulation) {
+        return null;
+    }
+    public VMGame parseGameToVMGame(Game game) { return null; }
+    public VMGroup parseGroupToVMGroup(Group group) { return null; }
+    public VMInitialization parseInitializationToVMInitialization(Initialization initialization) { return null; }
+    /**
+     * Übersetzt die Daten einer abgeschlossenen Simulation in ein für die Ergebnisansicht konformes Format.
+     *
+     * @param simulation die abgeschlossene Simulation
+     * @return die für die Ergebnisansicht aufbereiteten Ergebnisse
+     */
+    public VMResult parseSimulationToVMResult(Simulation simulation) {
+        return null;
+    }
+    public VMStrategy parseStrategyToVMStrategy(Strategy strategy) { return null; }
+
+
+    public CombinedStrategy parseVMCombinedStrategyToCombinedStrategy(VMCombinedStrategy vmCombinedStrategy) { return null; }
     /**
      * Übersetzt die Benutzereingaben zur Konfiguration in ein für das Ausführen der Simulation konformes Format.
      *
@@ -28,28 +52,13 @@ public class Communicator {
         return config;*/
         return null;
     }
+    public Game parseVMGameToGame(VMGame vmGame) { return null; }
+    public Group parseVMGroupToGroup(VMGroup vmGroup) { return null; }
+    public Initialization parseVMInitializationToInitialization(VMInitialization vmInitialization) { return null; }
+    public Strategy parseVMStrategyToStrategy(VMStrategy vmStrategy) { return null; }
 
-    /**
-     * Gibt zu einer Simulation die zugrundeliegende Konfiguration in einem für die Benutzeroberfläche konformen Format zurück.
-     *
-     * @param simulation die Simulation
-     * @return die der Simulation zugrundeliegende Konfiguration
-     */
-    public VMConfiguration parseSimulationToVMConfiguration(Simulation simulation) {
-        return null;
-    }
 
-    /**
-     * Übersetzt die Daten einer abgeschlossenen Simulation in ein für die Ergebnisansicht konformes Format.
-     *
-     * @param simulation die abgeschlossene Simulation
-     * @return die für die Ergebnisansicht aufbereiteten Ergebnisse
-     */
-    public VMResult parseSimulationToVMResult(Simulation simulation) {
-        return null;
-    }
 
-    //private Game parseVMGameToGame(VMGame vmGame){ }
-    //...
+
 
 }
