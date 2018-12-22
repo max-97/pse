61dc3af22bc998112e403abc1003b100075acbfb
Michel Bodé <michel.bode97@gmail.com>
2018-12-22 19:54:54 +0100

    Javadoc zu Game und Observable Simulation hinzugefügt

--- Implementierung/src/main/java/de/sswis/model/Game.java
+++ Implementierung/src/main/java/de/sswis/model/Game.java
@@ -2,7 +2,8 @@ package de.sswis.model;
 
 
 /**
- *
+ * Ein Stufenspiel, welches durch seine Payoffs definiert ist.
+ * Ein Spiel wird von zwei Agenten gespielt und, abhängig von deren Aktionen, erhalten diese Payoffs.
  * @author Michel Bodé
  */
 public class Game {
@@ -13,20 +14,20 @@ public class Game {
 
 
     /**
-     *
-     * @param name
-     * @param description
-     * @param payoffs
+     * Erstellt ein Stufenspiel.
+     * @param name Name des Spiels
+     * @param description Beschreibung des Spiels
+     * @param payoffs Payoffs des Spiels
      */
     public Game(String name, String description, Tuple[][] payoffs) {
 
     }
 
     /**
-     *
-     * @param a1
-     * @param a2
-     * @return
+     * Gibt die Payoffs zu bestimmten Aktionen zurück.
+     * @param a1 Aktion des ersten Agenten
+     * @param a2 Aktion des zweiten Agenten
+     * @return ein Paar von Payoffs
      */
     public Tuple getPayoffs(Action a1, Action a2) {
         return null;
--- Implementierung/src/main/java/de/sswis/model/ObservableSimulation.java
+++ Implementierung/src/main/java/de/sswis/model/ObservableSimulation.java
@@ -2,11 +2,28 @@ package de.sswis.model;
 
 import de.sswis.controller.SimulationObserver;
 
+/**
+ * Eine {@code ObservableSimulation} die von mehreren {@code SimulationObserver} beobachtet werden kann.
+ * Die {@code SimulationObserver} können so auf Änderungen des Zustands der {@code ObservableSimulation}
+ * hingewiesen werden.
+ * @author Michel Bodé
+ */
 public interface ObservableSimulation {
 
+    /**
+     * Fügt der {@code ObservableSimulation} einen Beobachter hinzu.
+     * @param o hinzuzufügender Beobachter
+     */
     void addObserver(SimulationObserver o);
 
+    /**
+     * Benachrichtigt alle Beobachter.
+     */
     void notifyObservers();
 
+    /**
+     * Entfernt einen Beobachter aus der {@code ObservableSimulation}.
+     * @param o zu entfernender Beobachter
+     */
     void deleteObserver(SimulationObserver o);
 }
