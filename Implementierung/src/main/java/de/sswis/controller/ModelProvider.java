20035fb09b6d616b8d373fe6f838c968b6214832
Max Braun <maxbraun97@web.de>
2018-12-22 14:52:43 +0100

    JavaDoc für ModelProvider

--- Implementierung/src/main/java/de/sswis/controller/ModelProvider.java
+++ Implementierung/src/main/java/de/sswis/controller/ModelProvider.java
@@ -1,20 +1,25 @@
 package de.sswis.controller;
 
-import de.sswis.model.*;
+import de.sswis.model.CombinedStrategy;
+import de.sswis.model.Configuration;
+import de.sswis.model.Game;
+import de.sswis.model.Initialization;
 
-import java.util.Collection;
 import java.util.HashMap;
-import java.util.Map;
 
 /**
+ * Verwaltet die Model-Objekte. Alle erstellten {@link Configuration Configurations}, {@link Game Games},
+ * {@link CombinedStrategy CombinedStrategies} und {@link Initialization Initializatoins} werden hier gespeichert, damit
+ * andere Controller-Objekte auf sie zugreifen können.
+ * Alle Objekte werden über ihren zugewiesenen Namen indentifiziert.
+ *
  * @author Max Braun
  */
 public class ModelProvider {
 
     private static ModelProvider modelProvider;
 
-    private HashMap<String, Collection<Configuration>> configurations;
-    private HashMap<Integer, Agent> agents;
+    private HashMap<String, Configuration> configurations;
     private HashMap<String, Game> games;
     private HashMap<String, CombinedStrategy> combinedStrategies;
     private HashMap<String, Initialization> initialization;
@@ -23,6 +28,11 @@ public class ModelProvider {
 
     }
 
+    /**
+     * Gibt die Instanz dieser Klasse zurück. Es wird immer die gleiche Instanz zurück gegeben.
+     *
+     * @return die Instanz dieser Klasse
+     */
     public static ModelProvider getInstance() {
         if (modelProvider == null) {
             modelProvider = new ModelProvider();
@@ -30,59 +40,91 @@ public class ModelProvider {
         return modelProvider;
     }
 
-    public void addConfigurations(Collection<Configuration> configurations) {
+    /**
+     * Fügt eine {@code Collection} hinzu.
+     *
+     * @param configuration die zu speichernde {@code Configurations}
+     */
+    public void addConfigurations(Configuration configuration) {
 
     }
 
-    public void addAgent(Agent agent) {
 
-    }
-
+    /**
+     * Fügt ein {@code Game} hinzu.
+     *
+     * @param game das zu speichernde {@code Game}
+     */
     public void addGame(Game game) {
 
     }
 
+
+    /**
+     * Fügt eine {@code CombinedStrategy} hinzu.
+     *
+     * @param combStrategy die zu speichernde {@code CombinedStrategy}
+     */
     public void addCombinedStrategy(CombinedStrategy combStrategy) {
 
     }
 
+
+    /**
+     * Fügt eine {@code Initialization} hinzu.
+     *
+     * @param initialization die zu speichernde {@code Initialization}
+     */
     public void addInitialization(Initialization initialization) {
 
     }
 
-    public void deleteConfigurations(String name) {
+    /**
+     * Löscht eine {@code Configuration}. Die {@code Configuration} wird über ihren Namen identifiziert.
+     * Löschen kann nicht rückgängig gemacht werden.
+     *
+     * @param name Name der zu löschenden {@code Configuration}
+     */
+    public void deleteConfiguration(String name) {
 
     }
 
-    public void deleteAgents(int id) {
-
-    }
-
+    /**
+     * Löscht ein {@code Game}. Das {@code Game} wird über seinen Namen identifiziert.
+     * Löschen kann nicht rückgängig gemacht werden.
+     *
+     * @param name Name des zu löschende {@code Game}
+     */
     public void deleteGame(String name) {
 
     }
 
+    /**
+     * Löscht eine {@code CombinedStrategy}. Die {@code CombinedStrategy} wird über ihren Namen identifiziert.
+     * Löschen kann nicht rückgängig gemacht werden.
+     *
+     * @param name Name der zu löschenden {@code CombinedStrategy}
+     */
     public void deleteCombinedStrategy(String name) {
 
     }
 
+    /**
+     * Löscht eine {@code Initialization}. Die {@code Initialization} wird über ihren Namen identifiziert.
+     * Löschen kann nicht rückgängig geamacht werden.
+     *
+     * @param name Name der zu löschenden {@code Initialization}
+     */
     public void deleteInitialization(String name) {
 
     }
 
+    /*
     public Map<String, Collection<Configuration>> getConfigurations() {
         return configurations;
     }
 
     public Configuration getConfiguration(String name) {
-        return null;
-    }
-
-    public Map<Integer, Agent> getAgents() {
-        return agents;
-    }
-
-    public Agent getAgent(int id) {
         return null;
     }
 
@@ -109,5 +151,6 @@ public class ModelProvider {
     public Initialization getInitialization(String name) {
         return null;
     }
+    */
 
 }
