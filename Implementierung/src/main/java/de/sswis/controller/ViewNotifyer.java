869a8240881b611b2df1dd5a35b3873bae3db538
Max Braun <maxbraun97@web.de>
2018-12-22 14:30:13 +0100

    JavaDoc für SimulationObserver und ViewNotifyer

--- Implementierung/src/main/java/de/sswis/controller/ViewNotifyer.java
+++ Implementierung/src/main/java/de/sswis/controller/ViewNotifyer.java
@@ -3,10 +3,21 @@ package de.sswis.controller;
 import de.sswis.model.Simulation;
 import de.sswis.view.AbstractMainView;
 
+/**
+ * Benachrichtig die View über beendete {@code Simulationen}. Wird eine Simulation beendet benachrichtigt dieser
+ * {@code SimulationObserver} die View. Dazu wird die entsprechende {@code Simulation} im Hauptfenster als
+ * abgeschlossen markiert.
+ *
+ * @author Max Braun
+ */
 public class ViewNotifyer implements SimulationObserver {
 
     private AbstractMainView mainView;
 
+    /**
+     * Konstruktor von {@code ViewNotifyer}
+     * @param mainView das Hauptfenster, das benachrichtig wird
+     */
     public ViewNotifyer(AbstractMainView mainView) {
 
     }
