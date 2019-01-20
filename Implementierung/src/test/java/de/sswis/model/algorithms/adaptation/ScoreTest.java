9b0ecd98be01c3bba344a98c2d3959fb0347c2a5
Michel Bodé <michel.bode97@gmail.com>
2019-01-20 21:47:05 +0100

    ScoreTest hinzugefügt

--- Implementierung/src/test/java/de/sswis/model/algorithms/adaptation/ScoreTest.java
+++ Implementierung/src/test/java/de/sswis/model/algorithms/adaptation/ScoreTest.java
@@ -1,5 +1,38 @@
 package de.sswis.model.algorithms.adaptation;
 
+import de.sswis.model.Agent;
+import de.sswis.model.algorithms.ranking.Score;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+import java.util.HashMap;
+
 public class ScoreTest {
 
+    private static Agent[] agents;
+    private static Score rankingAlgorithm;
+
+    @Before
+    public void init() {
+        agents = new Agent[10];
+        rankingAlgorithm = new Score();
+
+        for(int i = 0; i < 10; i++) {
+            agents[i] = new Agent(i + 1, 0, null, null);
+        }
+
+        for(int i = 0; i < agents.length; i++) {
+            agents[i].getHistory().increaseRoundCount();
+            agents[i].getHistory().setScore(10 - i);
+        }
+    }
+
+    @Test
+    public void rankingShouldBeCorrect() {
+        HashMap<Agent, Integer> ranking = rankingAlgorithm.getRankings(agents);
+        for(int i = 0; i < 10; i++) {
+            assertEquals((int) ranking.get(agents[i]), agents[i].getId());
+        }
+    }
 }
