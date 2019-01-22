63397ba212cb93477824ad41abcb51b744771d98
Michel Bodé <michel.bode97@gmail.com>
2019-01-22 15:13:09 +0100

    RankPercentage implementiert

--- Implementierung/src/main/java/de/sswis/model/algorithms/adaptation/RankPercentage.java
+++ Implementierung/src/main/java/de/sswis/model/algorithms/adaptation/RankPercentage.java
@@ -3,11 +3,12 @@ package de.sswis.model.algorithms.adaptation;
 import de.sswis.model.Agent;
 
 import java.util.HashMap;
+import java.util.Random;
 
 /**
  * Ein Algorithmus der die Strategie eines Agenten anpasst, wenn der Agent mit dem verglichen wird
  * zu den obersten {@code PERCENTAGE} Prozent der Rangliste gehoert. Der Agent uebernimmt die Strategie des anderen,
- * falls diese Kondition erfuellt ist. Der Rang eines Agenten wird nicht angepasst, wenn der Rang des Agenten mit
+ * falls diese Kondition erfuellt ist. Die Strategie eines Agenten wird nicht angepasst, wenn der Rang des Agenten mit
  * dem verglichen wird tiefer ist.
  * @author Michel Bodé
  */
@@ -17,6 +18,7 @@ public class RankPercentage implements AdaptationAlgorithm {
     public static final int PARAMETER_COUNT = 0;
     public static final String[] PARAMETER_NAMES = {};
     private final int PERCENTAGE;
+    private Random rnd;
 
     /**
      * Konstruktor
@@ -28,6 +30,23 @@ public class RankPercentage implements AdaptationAlgorithm {
 
     @Override
     public void adapt(Agent[] agents, HashMap<Agent, Integer> currentRanking, double adaptationProbability) {
+        int cutOff = (int) Math.ceil(agents.length*(double)PERCENTAGE/100);
+        rnd = new Random();
 
+        for(int i = 0; i < agents.length; i++) {
+            int rndInt = rnd.nextInt(101);
+            if(rndInt < adaptationProbability*100) {
+                Agent randomAgent = getRandomAgent(agents);
+                if(currentRanking.get(randomAgent) < cutOff && currentRanking.get(randomAgent) < currentRanking.get(agents[i])) {
+                    agents[i].setStrategy(randomAgent.getStrategy());
+                }
+
+            }
+        }
+    }
+
+    private Agent getRandomAgent(Agent[] agents) {
+        double rndDouble = (double)rnd.nextInt(101)/100;
+        return agents[(int)Math.floor(agents.length*rndDouble)];
     }
 }
