d097cc7a326a5ca6a567013e9c02e960164fb6c4
Michel Bodé <michel.bode97@gmail.com>
2019-01-25 18:58:48 +0100

    Weitere Tests hinzugefügt

--- Implementierung/src/test/java/de/sswis/model/algorithms/ranking/CurrentCycleScoreTest.java
+++ Implementierung/src/test/java/de/sswis/model/algorithms/ranking/CurrentCycleScoreTest.java
@@ -11,12 +11,12 @@ import static org.junit.Assert.assertEquals;
 public class CurrentCycleScoreTest {
 
     private static Agent[] agents;
-    private static Score rankingAlgorithm;
+    private static CurrentCycleScore rankingAlgorithm;
 
     @Before
     public void init() {
         agents = new Agent[4];
-        rankingAlgorithm = new Score();
+        rankingAlgorithm = new CurrentCycleScore();
 
         for(int i = 0; i < agents.length; i++) {
             agents[i] = new Agent(i + 1, 0, null, null);
@@ -25,15 +25,15 @@ public class CurrentCycleScoreTest {
 
     @Test
     public void getRankingsTest1() {
-        agents[0].setScore(10);
-        agents[1].setScore(5);
-        agents[2].setScore(5);
-        agents[3].setScore(7);
+        agents[0].setScore(0);
+        agents[1].setScore(30);
+        agents[2].setScore(70);
+        agents[3].setScore(100);
 
         for(int i = 0; i < agents.length; i++) {
             agents[i].getHistory().increaseCycleCount();
             agents[i].getHistory().setScore(agents[i].getScore());
-            agents[i].setScore(10 - i);
+            agents[i].setScore(agents[i].getScore() + 10 - i);
         }
 
         HashMap<Agent, Integer> ranking = rankingAlgorithm.getRankings(agents);
@@ -41,6 +41,20 @@ public class CurrentCycleScoreTest {
         assertEquals((int) ranking.get(agents[1]), 2);
         assertEquals((int) ranking.get(agents[2]), 3);
         assertEquals((int) ranking.get(agents[3]), 4);
+    }
+
+    @Test
+    public void getRankingsTest2() {
+        agents[0].setScore(10);
+        agents[1].setScore(5);
+        agents[2].setScore(5);
+        agents[3].setScore(7);
+
+        HashMap<Agent, Integer> ranking = rankingAlgorithm.getRankings(agents);
+        assertEquals((int) ranking.get(agents[0]), 1);
+        assertEquals((int) ranking.get(agents[1]), 3);
+        assertEquals((int) ranking.get(agents[2]), 3);
+        assertEquals((int) ranking.get(agents[3]), 2);
     }
 
 }
--- Implementierung/src/test/java/de/sswis/model/algorithms/ranking/CustomCycleScoreTest.java
+++ Implementierung/src/test/java/de/sswis/model/algorithms/ranking/CustomCycleScoreTest.java
@@ -2,19 +2,65 @@ package de.sswis.model.algorithms.ranking;
 
 import de.sswis.model.Agent;
 import org.junit.Before;
+import org.junit.Test;
+
+import java.util.HashMap;
+
+import static org.junit.Assert.assertEquals;
 
 public class CustomCycleScoreTest {
 
     private static Agent[] agents;
-    private static Score rankingAlgorithm;
+    private static CustomCycleScore rankingAlgorithm;
 
     @Before
     public void init() {
         agents = new Agent[4];
-        rankingAlgorithm = new Score();
+    }
 
+    @Test
+    public void getRankingsTest1() {
+
+        for(int i = 0; i < agents.length; i++) {
+            agents[i] = new Agent(i + 1, i*100, null, null);
+        }
+
+        rankingAlgorithm = new CustomCycleScore(2);
+
+        for(int i = 0 ; i < 3; i++) {
+            agents[0].setScore(agents[0].getScore() + 10);
+            agents[1].setScore(agents[1].getScore() + 5);
+            agents[2].setScore(agents[2].getScore() + 5);
+            agents[3].setScore(agents[3].getScore() + 1);
+
+            for(int j = 0; j < agents.length; j++) {
+                agents[j].getHistory().increaseCycleCount();
+                agents[j].getHistory().setScore(agents[j].getScore());
+            }
+        }
+
+        HashMap<Agent, Integer> ranking = rankingAlgorithm.getRankings(agents);
+        assertEquals((int) ranking.get(agents[0]), 1);
+        assertEquals((int) ranking.get(agents[1]), 2);
+        assertEquals((int) ranking.get(agents[2]), 2);
+        assertEquals((int) ranking.get(agents[3]), 3);
+    }
+
+    @Test
+    public void getRankingsTest2() {
         for(int i = 0; i < agents.length; i++) {
             agents[i] = new Agent(i + 1, 0, null, null);
         }
+
+        agents[0].setScore(10);
+        agents[1].setScore(5);
+        agents[2].setScore(5);
+        agents[3].setScore(7);
+
+        HashMap<Agent, Integer> ranking = rankingAlgorithm.getRankings(agents);
+        assertEquals((int) ranking.get(agents[0]), 1);
+        assertEquals((int) ranking.get(agents[1]), 3);
+        assertEquals((int) ranking.get(agents[2]), 3);
+        assertEquals((int) ranking.get(agents[3]), 2);
     }
 }
