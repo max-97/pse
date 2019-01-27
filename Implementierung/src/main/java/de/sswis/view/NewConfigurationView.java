de7dea34712fa266e91282a544f17f6e41601d01
sophie <sophiebraeuniger@gmail.com>
2019-01-27 10:40:45 +0100

    NewConfigurationView Konstruktor, setter, VMConfig anzeigen implementiert

--- Implementierung/src/main/java/de/sswis/view/NewConfigurationView.java
+++ Implementierung/src/main/java/de/sswis/view/NewConfigurationView.java
@@ -3,12 +3,12 @@ package de.sswis.view;
 import com.intellij.uiDesigner.core.GridConstraints;
 import com.intellij.uiDesigner.core.GridLayoutManager;
 import com.intellij.uiDesigner.core.Spacer;
-import de.sswis.view.CustomComponents.ParameterTable;
 import de.sswis.view.model.*;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -20,33 +20,43 @@ public class NewConfigurationView implements AbstractNewConfigurationView {
 
     private JFrame frame;
 
+    private AbstractManageConfigurationsView parentView;
 
     private VMConfiguration vmConfiguration;
 
     private List<String> adaptationAlgs;
     private List<String> pairingAlgs;
     private List<String> rankingAlgs;
-    private List<VMGame> games;
-    private List<VMInitialization> inits;
-    private List<VMStrategy> strategies;
-    private List<VMCombinedStrategy> cStrategies;
+    private List<String> games;
+    private List<String> inits;
+    private List<String> strategies;
+    private List<String> cStrategies;
 
 
-    private JComboBox comboBox1;
-    private JComboBox comboBox2;
-    private JComboBox comboBox3;
-    private JComboBox comboBox4;
-    private JComboBox comboBox5;
-    private JTextField textField1;
-    private JTextField textField2;
-    private JTextField textField3;
-    private JButton fertigButton;
-    private JButton abbrechenButton;
+    private JComboBox gameComboBox;
+    private JComboBox initComboBox;
+    private JComboBox pairingComboBox;
+    private JComboBox rankingComboBox;
+    private JComboBox adaptionComboBox;
+    private JTextField adaptionProbabilityTextField;
+    private JTextField roundsTextField;
+    private JTextField cyclesTextField;
+    private JButton finishButton;
+    private JButton cancelButton;
     private JScrollPane pairingAlgoParamPane;
     private JPanel ContentPane;
     private JScrollPane rankingAlgoParamPane;
     private JScrollPane adaptionAlgoParamPane;
 
+    public NewConfigurationView() {
+        adaptationAlgs = new ArrayList<String>();
+        pairingAlgs = new ArrayList<String>();
+        rankingAlgs = new ArrayList<String>();
+        games = new ArrayList<String>();
+        inits = new ArrayList<String>();
+        strategies = new ArrayList<String>();
+        cStrategies = new ArrayList<String>();
+    }
 
     @Override
     public void update() {
@@ -70,15 +80,16 @@ public class NewConfigurationView implements AbstractNewConfigurationView {
 
     @Override
     public void addCancelButtonActionlistener(ActionListener listener) {
+        cancelButton.addActionListener(listener);
 
     }
 
     @Override
     public void addFinishButtonActionlistener(ActionListener listener) {
-
+        finishButton.addActionListener(listener);
     }
 
-    private AbstractManageConfigurationsView parentView;
+
 
 
     @Override
@@ -93,27 +104,27 @@ public class NewConfigurationView implements AbstractNewConfigurationView {
 
     @Override
     public void addAdaptionAlgorithm(String name) {
-
+        adaptationAlgs.add(name);
     }
 
     @Override
     public void addPairingAlgorithm(String name) {
-
+        pairingAlgs.add(name);
     }
 
     @Override
     public void addRankingAlgorithm(String name) {
-
+        rankingAlgs.add(name);
     }
 
     @Override
     public void addInitialization(String name) {
-
+        inits.add(name);
     }
 
     @Override
     public void addGame(String name) {
-
+        games.add(name);
     }
 
     @Override
@@ -158,41 +169,41 @@ public class NewConfigurationView implements AbstractNewConfigurationView {
         final JLabel label3 = new JLabel();
         label3.setText("Algorithmus zum Bilden von Paaren");
         panel1.add(label3, new GridConstraints(6, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
-        comboBox3 = new JComboBox();
-        panel1.add(comboBox3, new GridConstraints(6, 1, 1, 2, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        pairingComboBox = new JComboBox();
+        panel1.add(pairingComboBox, new GridConstraints(6, 1, 1, 2, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
         final JLabel label4 = new JLabel();
         label4.setText("Bewertungsalgorithmus");
         panel1.add(label4, new GridConstraints(9, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
-        comboBox4 = new JComboBox();
-        panel1.add(comboBox4, new GridConstraints(9, 1, 1, 2, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        rankingComboBox = new JComboBox();
+        panel1.add(rankingComboBox, new GridConstraints(9, 1, 1, 2, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
         final JLabel label5 = new JLabel();
         label5.setText("Anpassungsalgorithmus");
         panel1.add(label5, new GridConstraints(12, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
-        comboBox5 = new JComboBox();
-        panel1.add(comboBox5, new GridConstraints(12, 1, 1, 2, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        adaptionComboBox = new JComboBox();
+        panel1.add(adaptionComboBox, new GridConstraints(12, 1, 1, 2, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
         final JLabel label6 = new JLabel();
         label6.setText("Wahrscheinlichkeit, dass Agent seine Strategie anpasst");
         panel1.add(label6, new GridConstraints(14, 0, 1, 2, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
-        textField1 = new JTextField();
-        panel1.add(textField1, new GridConstraints(14, 2, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(150, -1), null, 0, false));
+        adaptionProbabilityTextField = new JTextField();
+        panel1.add(adaptionProbabilityTextField, new GridConstraints(14, 2, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(150, -1), null, 0, false));
         final JLabel label7 = new JLabel();
         label7.setText("Anzahl der Runden");
         panel1.add(label7, new GridConstraints(17, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
         final JLabel label8 = new JLabel();
         label8.setText("Maximale Anzahl der Zyklen");
         panel1.add(label8, new GridConstraints(18, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
-        textField2 = new JTextField();
-        panel1.add(textField2, new GridConstraints(17, 2, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(150, -1), null, 0, false));
-        textField3 = new JTextField();
-        panel1.add(textField3, new GridConstraints(18, 2, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(150, -1), null, 0, false));
+        roundsTextField = new JTextField();
+        panel1.add(roundsTextField, new GridConstraints(17, 2, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(150, -1), null, 0, false));
+        cyclesTextField = new JTextField();
+        panel1.add(cyclesTextField, new GridConstraints(18, 2, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(150, -1), null, 0, false));
         final JSeparator separator1 = new JSeparator();
         panel1.add(separator1, new GridConstraints(4, 0, 1, 3, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null, 0, false));
         final JSeparator separator2 = new JSeparator();
         panel1.add(separator2, new GridConstraints(15, 0, 1, 3, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null, 0, false));
-        comboBox1 = new JComboBox();
-        panel1.add(comboBox1, new GridConstraints(1, 1, 1, 2, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
-        comboBox2 = new JComboBox();
-        panel1.add(comboBox2, new GridConstraints(3, 1, 1, 2, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        gameComboBox = new JComboBox();
+        panel1.add(gameComboBox, new GridConstraints(1, 1, 1, 2, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        initComboBox = new JComboBox();
+        panel1.add(initComboBox, new GridConstraints(3, 1, 1, 2, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
         final Spacer spacer1 = new Spacer();
         panel1.add(spacer1, new GridConstraints(8, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_VERTICAL, 1, GridConstraints.SIZEPOLICY_WANT_GROW, new Dimension(-1, 20), null, null, 0, false));
         final Spacer spacer2 = new Spacer();
@@ -216,14 +227,14 @@ public class NewConfigurationView implements AbstractNewConfigurationView {
         final JPanel panel2 = new JPanel();
         panel2.setLayout(new GridLayoutManager(1, 3, new Insets(0, 0, 0, 0), -1, -1));
         ContentPane.add(panel2, new GridConstraints(1, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
-        fertigButton = new JButton();
-        fertigButton.setText("fertig");
-        panel2.add(fertigButton, new GridConstraints(0, 2, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        finishButton = new JButton();
+        finishButton.setText("fertig");
+        panel2.add(finishButton, new GridConstraints(0, 2, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
         final Spacer spacer8 = new Spacer();
         panel2.add(spacer8, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, 1, null, null, null, 0, false));
-        abbrechenButton = new JButton();
-        abbrechenButton.setText("Abbrechen");
-        panel2.add(abbrechenButton, new GridConstraints(0, 1, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        cancelButton = new JButton();
+        cancelButton.setText("Abbrechen");
+        panel2.add(cancelButton, new GridConstraints(0, 1, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
         label4.setLabelFor(rankingAlgoParamPane);
         label5.setLabelFor(adaptionAlgoParamPane);
     }
@@ -233,5 +244,41 @@ public class NewConfigurationView implements AbstractNewConfigurationView {
      */
     public JComponent $$$getRootComponent$$$() {
         return ContentPane;
+    }
+
+    private void createUIComponents() {
+        gameComboBox = new JComboBox(games.toArray());
+        if (vmConfiguration.getGame() != null) {
+            gameComboBox.setSelectedItem(vmConfiguration.getGame());
+        }
+
+        initComboBox = new JComboBox(inits.toArray());
+        if (vmConfiguration.getInit() != null) {
+            initComboBox.setSelectedItem(vmConfiguration.getInit());
+        }
+
+        pairingComboBox = new JComboBox(pairingAlgs.toArray());
+        if (vmConfiguration.getPairingAlg() != null) {
+            pairingComboBox.setSelectedItem(vmConfiguration.getPairingAlg());
+        }
+
+        rankingComboBox = new JComboBox(rankingAlgs.toArray());
+        if (vmConfiguration.getRankingAlg() != null) {
+            rankingComboBox.setSelectedItem(vmConfiguration.getRankingAlg());
+        }
+
+        adaptionComboBox = new JComboBox(adaptationAlgs.toArray());
+        if (vmConfiguration.getAdaptationAlg() != null) {
+            adaptionComboBox.setSelectedItem(vmConfiguration.getAdaptationAlg());
+        }
+
+        adaptionProbabilityTextField = new JTextField();
+        adaptionProbabilityTextField.setText(vmConfiguration.getAdaptationProbability());
+
+        roundsTextField = new JTextField();
+        roundsTextField.setText(vmConfiguration.getRounds());
+
+        cyclesTextField = new JTextField();
+        cyclesTextField.setText(vmConfiguration.getCycles());
     }
 }
