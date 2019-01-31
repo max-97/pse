6f718906b663a428270efc27b59687d7ac570cfe
Michel Bodé <michel.bode97@gmail.com>
2019-01-31 19:15:24 +0100

    Testfall erweitert und Fehler in Game behoben

--- Implementierung/src/test/java/de/sswis/model/GameTest.java
+++ Implementierung/src/test/java/de/sswis/model/GameTest.java
@@ -4,6 +4,7 @@ import de.sswis.model.conditions.Always;
 import de.sswis.model.conditions.Condition;
 import de.sswis.model.strategies.AlwaysCooperate;
 import de.sswis.model.strategies.BaseStrategy;
+import de.sswis.model.strategies.NeverCooperate;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -22,17 +23,17 @@ public class GameTest {
         Game.Tuple[][] payoffs = new Game.Tuple[2][2];
         payoffs[0][0] = new Game.Tuple(10, 10);
         payoffs[1][1] = new Game.Tuple(0, 0);
-        payoffs[0][1] = new Game.Tuple(5, 5);
-        payoffs[1][0] = new Game.Tuple(5, 5);
-        game = new Game("Game1", "10/5/5/0", payoffs);
+        payoffs[0][1] = new Game.Tuple(-5, 5);
+        payoffs[1][0] = new Game.Tuple(5, -5);
+        game = new Game("Game1", "", payoffs);
     }
 
     @Test
-    public void playGameTest1() {
-        CombinedStrategy strategy = new CombinedStrategy("AlwaysCooperate",
+    public void playGameTest() {
+        CombinedStrategy strategy1 = new CombinedStrategy("AlwaysCooperate",
                 new BaseStrategy[]{new AlwaysCooperate()}, new Condition[]{new Always()});
-        agent1.setStrategy(strategy);
-        agent2.setStrategy(strategy);
+        agent1.setStrategy(strategy1);
+        agent2.setStrategy(strategy1);
         Pair pair = new Pair(agent1, agent2);
         game.playGame(pair);
         assertEquals(10, agent1.getScore());
@@ -47,5 +48,22 @@ public class GameTest {
         assertTrue(agent1.getHistory().groupCooperatedLastTime(agent2.getGroup()));
         assertTrue(agent2.getHistory().groupCooperatedEveryTime(agent1.getGroup()));
         assertTrue(agent2.getHistory().groupCooperatedLastTime(agent1.getGroup()));
+
+        CombinedStrategy strategy2 = new CombinedStrategy("NeverCooperate",
+                new BaseStrategy[]{new NeverCooperate()}, new Condition[]{new Always()});
+        agent2.setStrategy(strategy2);
+        game.playGame(pair);
+        assertEquals(5, agent1.getScore());
+        assertEquals(15, agent2.getScore());
+        assertTrue(agent1.getHistory().getAlwaysCooperated());
+        assertFalse(agent2.getHistory().getAlwaysCooperated());
+        assertFalse(agent1.getHistory().cooperatedEveryTime(agent2));
+        assertTrue(agent2.getHistory().cooperatedEveryTime(agent1));
+        assertFalse(agent1.getHistory().cooperatedLastTime(agent2));
+        assertTrue(agent2.getHistory().cooperatedLastTime(agent1));
+        assertTrue(agent1.getHistory().groupCooperatedEveryTime(agent2.getGroup()));
+        assertTrue(agent1.getHistory().groupCooperatedLastTime(agent2.getGroup()));
+        assertFalse(agent2.getHistory().groupCooperatedEveryTime(agent1.getGroup()));
+        assertFalse(agent2.getHistory().groupCooperatedLastTime(agent1.getGroup()));
     }
 }
