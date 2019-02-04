2019041fc39ce998ac3b0bb40bd5c64aa26c0ba1
Michel Bodé <michel.bode97@gmail.com>
2019-02-04 13:55:29 +0100

    Weiter Testfälle hinzugefügt

--- Implementierung/src/test/java/de/sswis/model/SimulationTest.java
+++ Implementierung/src/test/java/de/sswis/model/SimulationTest.java
@@ -10,7 +10,10 @@ import de.sswis.model.conditions.Always;
 import de.sswis.model.conditions.Condition;
 import de.sswis.model.strategies.AlwaysCooperate;
 import de.sswis.model.strategies.BaseStrategy;
+import de.sswis.model.strategies.NeverCooperate;
 import de.sswis.util.AgentDistribution;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -18,37 +21,84 @@ import static org.junit.Assert.*;
 
 public class SimulationTest {
 
-    @Test
-    public void test() {
-        CombinedStrategy strategy = new CombinedStrategy("Strategy", new BaseStrategy[]{new AlwaysCooperate()},
-                new Condition[]{new Always()});
-        Group group = new Group(1,"Group");
-        AgentDistribution distribution = new AgentDistribution(100);
-        Initialization init = new Initialization("Init", 10);
-        init.addGroup(group);
-        init.setGroupDistribution(distribution, group);
-        init.setStrategyDistribution(distribution, strategy, group);
+    private static Game game;
+    private static CombinedStrategy combinedStrategy1;
+    private static CombinedStrategy combinedStrategy2;
+    private static MixedStrategy mixedStrategy1;
+    private static RankingAlgorithm rankingAlgorithm;
+    private static AdaptationAlgorithm adaptationAlgorithm;
+    private static PairingAlgorithm pairingAlgorithm;
 
+
+    @BeforeClass
+    public static void init() {
         Game.Tuple[][] payoffs = new Game.Tuple[2][2];
         payoffs[0][0] = new Game.Tuple(10, 10);
         payoffs[1][1] = new Game.Tuple(0, 0);
         payoffs[0][1] = new Game.Tuple(-5, 5);
         payoffs[1][0] = new Game.Tuple(5, -5);
-        Game game = new Game("Game", "", payoffs);
-        AdaptationAlgorithm adaptationAlgorithm = new ReplicatorDynamicScore();
-        RankingAlgorithm rankingAlgorithm = new Score();
-        PairingAlgorithm pairingAlgorithm = new RandomPairing();
+        game = new Game("Game", "", payoffs);
+        combinedStrategy1 = new CombinedStrategy("Strategy", new BaseStrategy[]{new AlwaysCooperate()},
+                new Condition[]{new Always()});
+        combinedStrategy2 = new CombinedStrategy("Strategy2", new BaseStrategy[]{new NeverCooperate()},
+                new Condition[]{new Always()});
+        mixedStrategy1 = new MixedStrategy("Mixed Strategy", new CombinedStrategy[]{combinedStrategy1,
+                combinedStrategy2}, new double[]{0.5, 0.5});
+        adaptationAlgorithm = new ReplicatorDynamicScore();
+        rankingAlgorithm = new Score();
+        pairingAlgorithm = new RandomPairing();
+    }
+
+    @Test
+    public void testCombinedStrategy() {
+        Group group = new Group(1,"Group");
+        AgentDistribution distribution = new AgentDistribution(100);
+        Initialization init = new Initialization("Init", 10);
+        init.addGroup(group);
+        init.setGroupDistribution(distribution, group);
+        init.setStrategyDistribution(distribution, combinedStrategy1, group);
         Configuration config = new Configuration("Config", game, init, adaptationAlgorithm, pairingAlgorithm,
                 rankingAlgorithm, 100, 5, 1);
         Simulation sim = config.simulate();
         sim.run();
         Result result = sim.getResults();
-        assertEquals(1, result.getAgents().size());
 
         for(Agent agent: result.getAgents().get(1)) {
             assertTrue(agent.getHistory().getAlwaysCooperated());
             assertTrue(agent.getHistory().groupCooperatedEveryTime(group));
             assertTrue(agent.getHistory().groupCooperatedEveryTime(group));
         }
+    }
+
+    @Test
+    public void testMixedStrategy() {
+        Group group = new Group(1,"Group");
+        AgentDistribution distribution = new AgentDistribution(100);
+        Initialization init = new Initialization("Init", 10);
+        init.addGroup(group);
+        init.setGroupDistribution(distribution, group);
+        init.setStrategyDistribution(distribution, mixedStrategy1, group);
+        Configuration config = new Configuration("Config", game, init, adaptationAlgorithm, pairingAlgorithm,
+                rankingAlgorithm, 100, 5, 1);
+        Simulation sim = config.simulate();
+        sim.run();
+    }
+
+    @Test
+    public void testRepetitions() {
+        Group group = new Group(1,"Group");
+        AgentDistribution distribution = new AgentDistribution(100);
+        Initialization init = new Initialization("Init", 10);
+        init.addGroup(group);
+        init.setGroupDistribution(distribution, group);
+        init.setStrategyDistribution(distribution, combinedStrategy1, group);
+        Configuration config = new Configuration("Config", game, init, adaptationAlgorithm, pairingAlgorithm,
+                rankingAlgorithm, 100, 5, 1);
+        Simulation sim = config.simulate();
+        sim.setRepetitions(10);
+        sim.run();
+        Result result = sim.getResults();
+        assertEquals(10, result.getAgents().size());
+        assertEquals(10, result.getEquilibriums().size());
     }
 }
